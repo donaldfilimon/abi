46be0a52dc70cb9cd33974107a39ad5255702b99
Donald Filimon <cbkshadow@icloud.com>
2025-06-04 02:00:55 -0400

    Fix Zig build and main program

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "persona",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    if (b.args) |args| run_cmd.addArgs(args);
+    b.step("run", "Run the program").dependOn(&run_cmd.step);
+}
