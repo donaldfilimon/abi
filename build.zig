5f57812822d94c495cec53e802eb7d9b8172c1bf
Donald Filimon <cbkshadow@icloud.com>
2025-09-20 08:04:10 -0400

    build: run unit tests via run step

--- build.zig
+++ build.zig
@@ -26,7 +26,8 @@ pub fn build(b: *std.Build) void {
     const unit_tests = b.addTest(.{
         .root_module = main_module,
     });
+    const run_unit_tests = b.addRunArtifact(unit_tests);
 
     const test_step = b.step("test", "Run all tests");
-    test_step.dependOn(&unit_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
